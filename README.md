d4b2fdb667063fdc587ab5145db9e5146e08e628
Jakub Charvat <jakcharvat@gmail.com>
2021-03-11 00:46:51 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## IBDocuments Crawler
+
+This is a simple crawler to get past papers from [ibdocuments](https://ibdocuments.com). To get started download the binary from the [releases tab]() and run it. This will create a folder with the papers for the subject you chose in the same directory where the executable is (so most likely your downloads folder). This is a V1 so the downloading is pretty slow, either download in small batches or let it run overnight :)
+
+Also, eventually I wanna be able to crawl more stuff than just past papers, but again, V1. 
+
+If you want to check out the source code and contribute then it's that `crawl.ts` file (I know, the code is awful). If you find a bug please file an issue here in the repo or message me wherever you find me.
+
+Hope you find it helpful
+
+--Jakub
+
---
+++ crawl.ts
@@ -0,0 +1,227 @@
+import { DOMParser, Element, Node } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts'
+
+async function ask(
+    question = '',
+    stdin = Deno.stdin,
+    stdout = Deno.stdout,
+): Promise<string> {
+    const buf = new Uint8Array(1024)
+
+    // Write question to console
+    await stdout.write(new TextEncoder().encode(`\n\n${question}`))
+
+    // Read console input
+    const n = <number>await stdin.read(buf)
+    const answer = new TextDecoder().decode(buf.subarray(0, n))
+
+    return answer.trim()
+}
+
+async function shouldRunAgain(): Promise<boolean> {
+    const again =
+        (await ask('Would you like to run the script again?\n\n[y / n] > '))
+            .toLowerCase()
+    if (again === 'y') {
+        return true
+    } else if (again === 'n') {
+        return false
+    } else {
+        return await shouldRunAgain()
+    }
+}
+
+function sleep(milliseconds: number) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+async function getPastPapers() {
+    // Get Groups
+    const groupsUrl = 'https://www.ibdocuments.com/IB%20PAST%20PAPERS%20-%20SUBJECT/'
+
+    const groupsPage = await fetch(groupsUrl)
+    const groupsDom = new DOMParser().parseFromString(
+        await groupsPage.text(),
+        'text/html',
+    )
+    if (groupsDom === null) {
+        console.error('Error fetching from ibdocuments')
+        return
+    }
+
+    const groupEls = Array.from(groupsDom.querySelectorAll('a'))
+        .filter((el: Node) => {
+            return el.textContent.toLowerCase().includes('group')
+        })
+
+    const groups = groupEls
+        .map((el: Node, idx: number) =>
+            `${idx + 1}: ${el.textContent.replace(/\/$/g, '')}`
+        )
+        .map((el: string) => el.replace(/Group [0-9] - /g, ''))
+
+    const groupsLinks = groupEls
+        .map((el: Node) => groupsUrl + (el as Element).getAttribute('href'))
+
+    const groupsIdxs = groups.map((_, idx: number) => `${idx + 1}`)
+
+    const group = await ask(
+        `Select a group:\n${groups.join('\n')}\n\n[1...${groupsIdxs.length}] > `,
+    )
+
+    const groupIdx = groupsIdxs.indexOf(group)
+    if (groupIdx < 0 || groupIdx >= groupsLinks.length) {
+        console.error('Invalid Group')
+        return
+    }
+
+    const grouplink = groupsLinks[groupIdx]
+
+    // Get Subjects
+    const subjectsPage = await fetch(grouplink)
+    const subjectsDom = new DOMParser().parseFromString(
+        await subjectsPage.text(),
+        'text/html',
+    )
+
+    if (subjectsDom === null) {
+        console.error('Failed to get exams')
+        return
+    }
+
+    const subjectsCssString = 'table#indexlist tr:not(.even-parentdir) td.indexcolname a'
+    const subjectsEls = Array.from(subjectsDom.querySelectorAll(subjectsCssString))
+        .map((el: Node) => (el as Element))
+
+    const subjects = subjectsEls
+        .map((el: Element) => el.textContent)
+        .map((sub: string) => sub.replace(/_/g, ' '))
+        .map((sub: string) => sub.replace(/\/$/g, ''))
+        .map((sub: string, idx: number) => `${idx + 1}: ${sub}`)
+
+    const subjectsIdxs = subjectsEls
+        .map((_ , idx: number) => `${idx + 1}`)
+
+    const subjectsLinks = subjectsEls
+        .map((el: Element) => grouplink + el.getAttribute('href'))
+
+    const subject = await ask(`Select a subject:\n${subjects.join('\n')}\n\n[1...${subjects.length}] > `)
+    const subjectIdx = subjectsIdxs.indexOf(subject)
+    if (subjectIdx < 0 || subjectIdx >= subjects.length) {
+        console.error('Invalid subject')
+        return
+    }
+
+    const subjectLink = subjectsLinks[subjectIdx]
+
+
+
+    // Get Exams
+    const examsPage = await fetch(subjectLink)
+    const examsDom = new DOMParser().parseFromString(
+        await examsPage.text(),
+        'text/html',
+    )
+
+    if (examsDom === null) {
+        console.error('Failed to get exams')
+        return
+    }
+
+    const examsCssString = 'table#indexlist tr:not(.even-parentdir) td.indexcolname a'
+    const examsEls = Array.from(examsDom.querySelectorAll(examsCssString))
+        .map((el: Node) => (el as Element))
+
+    const exams = examsEls
+        .map((el: Element) => el.textContent)
+        .map((sub: string) => sub.replace(/_/g, ' '))
+        .map((sub: string) => sub.replace(/\/$/g, ''))
+        .map((sub: string, idx: number) => `${idx + 1}: ${sub}`)
+
+    const examsIdxs = examsEls
+        .map((_ , idx: number) => `${idx + 1}`)
+
+    const examsLinks = examsEls
+        .map((el: Element) => subjectLink + el.getAttribute('href'))
+
+    const exam = await ask(`What's the earliest exam you want to download?\n${exams.join('\n')}\n\n[1...${exams.length}] > `)
+    const examIdx = examsIdxs.indexOf(exam)
+    if (examIdx < 0 || examIdx >= exams.length) {
+        console.error('Invalid subject')
+        return
+    }
+
+    const dirname = subjects[subjectIdx].replace(/^[0-9]+: /g, '')
+    const p = Deno.run({
+        cmd: ['mkdir', `${dirname}`]
+    })
+    await p.status()
+    p.close()
+
+    for (let idx = examIdx; idx < exams.length; idx++) {
+        const examName = exams[idx].replace(/^[0-9]+: /g, '')
+        console.log(`Downloading "${examName}"...`)
+        const link = examsLinks[idx]
+
+        const sessionPage = await fetch(link)
+        const sessionDom = new DOMParser().parseFromString(
+            await sessionPage.text(), 
+            'text/html'
+        )
+
+        if (sessionDom === null) {
+            console.log('failed')
+            return
+        }
+        
+        const p = Deno.run({
+            cwd: dirname,
+            cmd: ['mkdir', examName]
+        })
+        await p.status()
+        p.close()
+
+        const selector = 'table#indexlist tr:not(.even-parentdir) td.indexcolname a'
+        const papers = Array.from(sessionDom.querySelectorAll(selector))
+        
+        for (const paper of papers) {
+            const el = paper as Element
+            const href = link + el.getAttribute('href')
+
+            const p = Deno.run({
+                cwd: `${dirname}/${examName}`,
+                cmd: ['curl', '-O', href],
+                stdout: "null"
+            })
+            await p.status()
+            p.close()
+        }
+
+        console.log('done')
+    }
+}
+
+// async function getDataBooklets() {
+// }
+
+while (true) {
+    // const res = await ask(
+    //     '\n\n\nWhat would you like to download?\n1) Past papers\n2) Data and Formula booklets\n\n[1 / 2] > ',
+    // )
+
+    // if (res === '1') {
+    //     await getPastPapers()
+    // } else if (res === '2') {
+    //     await getDataBooklets()
+    // } else {
+    //     console.log('Invalid Input')
+    // }
+
+
+    await getPastPapers()
+
+    if (await shouldRunAgain()) {
+        continue
+    }
+
+    break
+}
